e09d22e0aa6c9134e79f7fa66999353d6d0d9ffa
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-03-10 21:53:53 -0600

    Completed lab

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function add(a, b){
+    return a + b
+}
+
+function subtract (a, b){
+    return a - b
+}
+
+function multiply (a, b){
+    return a * b
+}
+
+function divide (a, b){
+    return a / b
+}
+
+function increment(n){
+    return 1 + n++
+}
+
+function decrement(n){
+    return n-- - 1
+}
+
+function makeInt(n="2"){
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n = "2.222"){
+    return parseFloat(n)
+}
